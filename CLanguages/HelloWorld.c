d15bbd01eea11ddd65710c0e11fa653b5656d8f0
DEBABRATA BAR <48706465+debabratabar@users.noreply.github.com>
2020-04-23 21:33:33 +0530

    Simplr hello world program

---
+++ CLanguages/HelloWorld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	char string[100];
+    gets(string);
+    printf("Hello, World! \n%s",string);
+  	
+    
+    return 0;
+}
+
