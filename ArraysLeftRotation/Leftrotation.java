233f9226d2f1321ea8a8839965ff6980f92ccf1e
DEBABRATA BAR <48706465+debabratabar@users.noreply.github.com>
2020-06-25 19:53:00 +0530

    left  rotation if array

---
+++ ArraysLeftRotation/Leftrotation.java
@@ -0,0 +1,37 @@
+package hackerrank;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Leftrotation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T=sc.nextInt();
+        for (int counter=0;counter<T;counter++){
+            int N= sc.nextInt();
+            int d= sc.nextInt();
+            int[] arr= new int[N];
+            for (int i =0;i<N;i++){
+                arr[i]=sc.nextInt();
+            }
+            int[] Resultarray= new int[N];
+            int position=0;
+            for ( int i=0;i<N;i++){
+                position= i-d;
+                if (position<0){
+                    position+=1;
+                    Resultarray[N-1+position]=arr[i];
+
+                }
+                else{
+                    Resultarray[i-d]=arr[i];
+                }
+            }
+            for (int element :Resultarray
+            ) {
+                System.out.print(element+" ");
+
+            }
+        }
+    }
+    }
